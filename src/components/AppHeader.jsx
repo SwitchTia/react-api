4308370b1b6c1939c28e71c1be0670dcb13d17f8
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-03 13:41:54 +0100

    Feat: General settings

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,8 @@
+export default function AppHeader() {
+
+    return (
+        <header className="flex-center">
+            <h1>Wonderful cast</h1>
+        </header>
+    );
+}
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,23 @@
+
+
+function AppMain() {
+
+
+
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <div className="text-center">
+                        <h1>Actors</h1>
+                        <p>List of actors fetched from an API</p>
+                    </div>
+
+
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default AppMain;
